90900eca4a38b023f77e697b097789de9f89b9f7
Rajganez <ganezsouth@live.com>
2024-08-04 22:46:29 +0530

    Corrections on file upload

--- server/routes/ChatRoutes.js
+++ server/routes/ChatRoutes.js
@@ -18,31 +18,14 @@ import multer from "multer";
 export const chatRoutes = Router();
 export const groupChatRouter = Router();
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    const uploadPath = path.join('/tmp/uploads');
-    cb(null, uploadPath);
-  },
-  filename: function (req, file, cb) {
-    cb(null, `${Date.now()}-${file.originalname}`);
-  }
-});
-const upload = multer({ storage: storage });
-// const upload = multer({ dest: "/tmp/uploads/files" });
+const upload = multer({ dest: "/tmp/uploads/files" });
 
 chatRoutes.get("/:id", getBuddies);
 chatRoutes.post("/search/:id", searchBuddies);
 chatRoutes.get("/fellow/:id", getFellowBuddy);
 chatRoutes.post("/directmessages", getSenderMsg);
 chatRoutes.post("/uploadfile", upload.single("file"), uploadFiles, (req, res) => {
-  try {
-    console.log(req.file);
-    const filePath = `/uploads/${req.file.filename}`;
-    res.status(200).send({ filepath: filePath });
-  } catch (error) {
-    console.error('Error handling file upload:', error);
-    res.status(500).send({ error: 'File upload failed' });
-  }
+  res.send({ filepath: req.file.location });
 });
 chatRoutes.post("/getchatcontacts", getChatContact);
 
